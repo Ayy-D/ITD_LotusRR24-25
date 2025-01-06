81d6af73f3ea74b244e2fc2174079a493333ec3a
Ayy-D <arnavdamle5@gmail.com>
2025-01-06 13:34:57 -0600

    New Programs: DRIVE_ONLY, Motor_Position_Test, Blue_Sample, Red_Sample, Red_Specimen still to be updated

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DRIVE_ONLY.java
@@ -0,0 +1,107 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.dfrobot.HuskyLens;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+@TeleOp(name="DRIVE_ONLY")
+
+public class DRIVE_ONLY extends LinearOpMode{
+
+    //Control Hub Motors
+    private DcMotorEx rightFront = null;
+    private DcMotorEx leftFront = null;
+    private DcMotorEx rightBack = null;
+    private DcMotorEx leftBack= null;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //drivetrain power vars
+        double  drive = 0;        // Desired forward power/speed (-1 to +1)
+        double  strafe = 0;        // Desired strafe power/speed (-1 to +1)
+        double  turn = 0;        // Desired turning power/speed (-1 to +1)
+
+        //drivetrain motors
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setDirection(DcMotorEx.Direction.REVERSE);
+
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setDirection(DcMotorEx.Direction.REVERSE);
+
+
+        waitForStart();
+        telemetry.addData(">>", "Press start to continue");
+        telemetry.update();
+        if (isStopRequested()) return;
+
+        while (opModeIsActive())
+        {
+
+            drive  = gamepad1.left_stick_y  / 1.8;  // Reduce drive rate to 66%.
+            strafe = gamepad1.left_stick_x  / 1.4;  // Reduce strafe rate to 100%.
+            turn   = -gamepad1.right_stick_x / 2;  // Reduce turn rate to 40%.
+            moveRobot(drive, strafe, turn);
+
+        }
+    }
+    public void moveRobot(double x, double y, double yaw) {
+        // Calculate wheel powers.
+        double leftFrontPower =  x -y -yaw;
+        double rightFrontPower =  x +y +yaw;
+        double leftBackPower =  x +y -yaw;
+        double rightBackPower =  x -y +yaw;
+
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Send powers to the wheels.
+
+        leftFront.setPower(leftFrontPower);
+        rightFront.setPower(rightFrontPower);
+        leftBack.setPower(leftBackPower);
+        rightBack.setPower(rightBackPower);
+
+    }
+}
