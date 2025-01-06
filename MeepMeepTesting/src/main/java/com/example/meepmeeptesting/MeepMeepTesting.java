0bdbf8ef90c702afa53f96545d8a6dbfea90f84c
Ayy-D <arnavdamle5@gmail.com>
2025-01-05 18:03:38 -0600

    Unfinished Blue & Red Spec, Migration to Acme MeepMeep, Test on MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,31 +1,75 @@
 package com.example.meepmeeptesting;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 
-import org.rowlandhall.meepmeep.MeepMeep;
-import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
-import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(600);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .build());
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 60, Math.toRadians(270)))
+                        //Preload Specimen Score
+                        .lineToY(34.5)
+
+                        //Move to grab floor spec 1
+                        .strafeToLinearHeading(new Vector2d(-34, 42), Math.toRadians(45))
+
+                        //Move to drop floor spec 1
+                        .strafeToLinearHeading(new Vector2d(-36, 48), Math.toRadians(315))
+
+                        //Move to grab floor spec 2
+                        .strafeToLinearHeading(new Vector2d(-44, 42), Math.toRadians(45))
+
+                        //Move to drop floor spec 2
+                        .strafeToLinearHeading(new Vector2d(-48, 48), Math.toRadians(315))
+
+                        //Move to grab floor spec 3
+                        .strafeToLinearHeading(new Vector2d(-54, 42), Math.toRadians(45))
+
+                        //Move to drop floor spec 3
+                        .strafeToLinearHeading(new Vector2d(-54, 48), Math.toRadians(315))
+
+                        //Move to grab spec 1 from wall
+                        .strafeToLinearHeading(new Vector2d(-40, 54), Math.toRadians(90))
+                        .strafeToLinearHeading(new Vector2d(-40, 58), Math.toRadians(90))
+
+                        //Move to score spec 1
+                        .strafeToSplineHeading(new Vector2d(0, 34), Math.toRadians(270))
+
+                        //Move to grab spec 2 from wall
+                        .strafeToLinearHeading(new Vector2d(-40, 54), Math.toRadians(90))
+                        .strafeToLinearHeading(new Vector2d(-40, 58), Math.toRadians(90))
+
+                        //Move to score spec 2
+                        .strafeToSplineHeading(new Vector2d(0, 34), Math.toRadians(270))
+
+                        //Move to grab spec 3 from wall
+                        .strafeToLinearHeading(new Vector2d(-40, 54), Math.toRadians(90))
+                        .strafeToLinearHeading(new Vector2d(-40, 58), Math.toRadians(90))
+
+                        //Move to score spec 3
+                        .strafeToSplineHeading(new Vector2d(0, 34), Math.toRadians(270))
+
+                        //Park
+                        .strafeToLinearHeading(new Vector2d(-24, 45), Math.toRadians(315))
 
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+
+                .build());
+
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_Specimen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_Specimen.java
@@ -27,8 +27,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 @Config
-@Autonomous(name = "Blue Specimen", group = "Autonomous")
-public class Blue_Specimen extends LinearOpMode {
+@Autonomous(name = "Red Specimen", group = "Autonomous")
+public class Red_Specimen extends LinearOpMode {
     ElapsedTime time = new ElapsedTime();
 
 
