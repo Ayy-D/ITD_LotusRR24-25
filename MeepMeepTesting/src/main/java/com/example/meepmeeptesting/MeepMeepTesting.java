2400ba0b817542c0c2487a95e757e3607995103d
Ayy-D <arnavdamle5@gmail.com>
2025-01-07 21:52:14 -0600

    used to redo the pathing, specimen auto positions are correct on this file

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -13,8 +13,8 @@ public class MeepMeepTesting {
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width (in)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15.5)
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 60, Math.toRadians(270)))
