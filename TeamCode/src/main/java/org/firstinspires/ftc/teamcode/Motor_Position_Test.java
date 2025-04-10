1baa8a2aa77d66f1574777a64f2f194c7855ff4c
aryan-damle <aryandamle9@gmail.com>
2025-04-10 00:29:42 -0500

    removed extra motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motor_Position_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motor_Position_Test.java
@@ -43,8 +43,6 @@ public class Motor_Position_Test extends LinearOpMode{
 
 
     //Expansion Hub Motors
-    private DcMotorEx rotR = null;
-    private DcMotorEx rotL = null;
     private DcMotorEx sL = null;
     private DcMotorEx sR = null;
 
@@ -66,32 +64,17 @@ public class Motor_Position_Test extends LinearOpMode{
     @Override
     public void runOpMode() throws InterruptedException {
 
-
-
-
-        //Linear Slide Rotation Motors
-        rotR = hardwareMap.get(DcMotorEx.class, "rotateR");
-        //rotR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rotR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rotR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rotR.setDirection(DcMotor.Direction.REVERSE);
-
-
-        rotL = hardwareMap.get(DcMotorEx.class, "rotateL");
-
-        //rotL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rotL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rotL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
         //LS Motors
         sL = hardwareMap.get(DcMotorEx.class, "slideL");
         sL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        sL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        sL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        sL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
         sR = hardwareMap.get(DcMotorEx.class, "slideR");
         sR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        sR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        sR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        sR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         sR.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
@@ -102,12 +85,6 @@ public class Motor_Position_Test extends LinearOpMode{
 
         while (opModeIsActive())
         {
-
-            telemetry.addData("rotR Position", rotR.getCurrentPosition());
-            telemetry.addData("rotR Current Draw", rotR.getCurrent(CurrentUnit.AMPS));
-            telemetry.addData("rotL Position", rotL.getCurrentPosition());
-            telemetry.addData("rotL Current Draw", rotL.getCurrent(CurrentUnit.AMPS));
-
 
             telemetry.addData("-----", placeholder);
 
